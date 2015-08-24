7e6dbc6a669c8b7246ca3265de2472b9e6a508cf
Guilherme Dutra <guipereira07@gmail.com>
2015-08-24 14:07:22 -0300

    Remove test nonsense

--- test/crawler_fetcher/distributor_test.clj
+++ test/crawler_fetcher/distributor_test.clj
@@ -8,14 +8,3 @@
              (split-repos [{:name "rails" :login "rails"}
                            {:name "linux" :login "linux"}
                            {:name "jquery" :login "jquery"}]) => [[{:name "rails" :login "rails"}] [{:name "linux" :login "linux"}] [{:name "jquery" :login "jquery"}]]))
-
-
-(facts "about 'distribute'"
-       (fact "it distribute the repositories for the servers"
-             (distribute [{:name "rails" :login "rails"}
-                          {:name "linux" :login "linux"}
-                          {:name "jquery" :login "jquery"}]) => '(nil nil nil)
-             (provided
-               (clj-http.client/post anything {:body "[{\"name\":\"rails\",\"login\":\"rails\"}]" :content-type :json :accept :json}) => nil
-               (clj-http.client/post anything {:body "[{\"name\":\"linux\",\"login\":\"linux\"}]" :content-type :json :accept :json}) => nil
-               (clj-http.client/post anything {:body "[{\"name\":\"jquery\",\"login\":\"jquery\"}]" :content-type :json :accept :json}) => nil)))
